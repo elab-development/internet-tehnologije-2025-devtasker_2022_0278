dd89997540cf06431673f1953bee122ca271dee0
Andrea <andrea.dorontic@gmail.com>
2026-01-08 21:08:20 +0100

    Dodatne migracije za sve tabele da napravimo

---
+++ devbe/database/migrations/2026_01_08_200348_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('project_id')->constrained('projects')->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained('tags')->restrictOnDelete();
+
+            $table->foreignId('created_by')->constrained('users')->restrictOnDelete();
+            $table->foreignId('assigned_to')->nullable()->constrained('users')->nullOnDelete();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+
+            $table->enum('status', ['created', 'started', 'in_progress', 'done'])->default('created');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+
+            $table->date('due_date')->nullable();
+
+            $table->timestamps();
+
+            $table->index(['project_id', 'status']);
+            $table->index(['assigned_to']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
