3b8ac35882e34cbf632111914415d26ad38e6e92
Andrea <andrea.dorontic@gmail.com>
2026-01-08 23:32:08 +0100

    Sideri za punjenje baze mock  podacima

---
+++ devbe/database/seeders/TagSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tag;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tags = [
+            'Bug',
+            'Feature',
+            'Frontend',
+            'Backend',
+            'DevOps',
+            'Testing',
+        ];
+
+        foreach ($tags as $name) {
+            Tag::create(['name' => $name]);
+        }
+    }
+}
---
+++ devbe/database/seeders/UserSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //admin
+        User::updateOrCreate(
+            ['email' => 'admin@devtasker.com'],
+            [
+                'name' => 'Admin',
+                'role' => 'taskadmin',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        // Product owner (Andrea).
+        User::updateOrCreate(
+            ['email' => 'andrea@devtasker.com'],
+            [
+                'name' => 'Andrea',
+                'role' => 'product_owner',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        // Developer (Jovana).
+        User::updateOrCreate(
+            ['email' => 'jovana@devtasker.com'],
+            [
+                'name' => 'Jovana',
+                'role' => 'developer',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        // Random 5 korisnika.
+        User::factory()
+            ->count(5)
+            ->create([
+                'role' => 'developer',
+            ]);
+   
+    }
+}
