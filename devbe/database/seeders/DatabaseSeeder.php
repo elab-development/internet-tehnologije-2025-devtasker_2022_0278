4eb27d7affee002dc14fa4e48cf3a4ec2eddfd0f
Jovana <sekulic2805@gmail.com>
2026-01-15 20:00:08 +0100

    Pozivamo na sedovanje baze u db seederu i provera mysql baze da je sve tu

--- devbe/database/seeders/DatabaseSeeder.php
+++ devbe/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       $this->call([
+        UserSeeder::class,
+        TagSeeder::class,
+        ProjectSeeder::class,
+        ProjectUserSeeder::class,
+        TaskSeeder::class,
+        CommentSeeder::class,
+    ]);
     }
 }
