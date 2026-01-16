7b5d139a9256983053e0c5ccfd5f7ab618928aab
Jovana <sekulic2805@gmail.com>
2026-01-16 21:12:25 +0100

    Sitna izmena u user modelu da mozemo tokene da koristimo

--- devbe/app/Models/User.php
+++ devbe/app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $fillable = [
         'name',
